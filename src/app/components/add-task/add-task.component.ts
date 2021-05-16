fbe92e53c3f4b123a0c9a030bfcfa3fb613b29cb
Aristotelis Palaiologopoulos <telis1@windowslive.com>
2021-05-16 18:46:28 +0200

    toggle form and routing

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+
 import { Task } from '../../Task';
 
 @Component({
@@ -11,7 +14,14 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder: boolean = false;
-  constructor() {}
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
